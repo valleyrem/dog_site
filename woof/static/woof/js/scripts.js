94bda54cf810f1b367e6d9f0e7c4783e0003a2a7
valleyrem <remrem2310@gmail.com>
2024-08-31 21:20:04 +0400

    changed policies

--- woof/static/woof/js/scripts.js
+++ woof/static/woof/js/scripts.js
@@ -11,26 +11,7 @@ document.addEventListener('click', function(event) {
     }
 });
 
-// Скрипт для применения и переключения темы
-document.addEventListener("DOMContentLoaded", function() {
-    // При начальной загрузке добавить класс темы из localStorage
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    document.body.classList.add(currentTheme + '-theme');
 
-    // Удалить класс .hidden после применения темы
-    document.body.classList.remove('hidden');
-
-    // Переключение темы по нажатию кнопки
-    document.querySelector('#theme-toggle').addEventListener('click', function() {
-        let newTheme = document.body.classList.contains('light-theme') ? 'dark' : 'light';
-        document.body.classList.remove('light-theme', 'dark-theme');
-        document.body.classList.add(newTheme + '-theme');
-        localStorage.setItem('theme', newTheme);
-    });
-});
-
-// Скрыть контент до применения темы
-document.body.classList.add('hidden');
 
 // Скрипт для кнопки "Прокрутить наверх"
 document.addEventListener('DOMContentLoaded', function() {
